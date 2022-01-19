3261ad691720d0aed509c6c78ab948edc7ca8d8d
SSUNGOK <>
2022-01-19 18:08:02 +0900

    init instagram

---
+++ instagram/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './App.css';
+import Header from './components/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
