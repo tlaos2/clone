3676bad179b1ebce59b04c0911628affb2d4f8b3
SSUNGOK <>
2022-01-21 17:37:42 +0900

    code it

---
+++ dice/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+  <h1 id='title'>가위바위보</h1>
+    <button className='hand'>가위</button>
+    <button className='hand'>바위</button>
+    <button className='hand'>보위</button>
+  </>,
+  document.getElementById('root')
+);
