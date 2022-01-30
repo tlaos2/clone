110765ffc9607fd0116c3e9d10cf3627e7693f0f
IamGrooot <tlaos2@naver.com>
2022-01-30 11:59:48 +0900

    logout

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,4 +1,13 @@
+import { authService } from 'fbase';
 import React from 'react';
 
-const Profile =()=> <span>Profile</span>;
-export default Profile;
+export default () => {
+  const onLogOutClick = () => {
+    authService.signOut();
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
